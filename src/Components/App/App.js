7dd0ee2de6990953c6ba1504a9a9f4a7499f7611
Charles Yach <CharlesY712@gmail.com>
2018-03-20 15:37:38 -0600

    Implements link and route from router

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { Route, NavLink, Link } from "react-router-dom";
+import "./App.css";
+import CardContainer from "../CardContainer/CardContainer";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      person: [{},{}]
+    }
+  }
+
   render() {
     return (
       <div className="App">
-          <h1 className="App-title">Welcome to React</h1>
+        <Link to="/">
+          <h1 className="App-title">SwapiBox</h1>
+        </Link>
+        <NavLink to="/people">People</NavLink>
+        <NavLink to="/vehicles">Vehicles</NavLink>
+        <NavLink to="/planets">Planets</NavLink>
+        <Route path="/people" render={ () => <CardContainer people={this.state.person} />} />
       </div>
     );
   }
