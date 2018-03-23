f09f3dfbcbe1d96ed42e6fca1d70f0f87705f6fa
Charles Yach <CharlesY712@gmail.com>
2018-03-23 08:44:09 -0600

    Resolving fetchPlanet promise return

--- src/Components/API/fetchPlanets.js
+++ src/Components/API/fetchPlanets.js
@@ -2,21 +2,22 @@ const fetchPlanets = async () => {
   const url = 'https://swapi.co/api/planets/';
   const raw = await fetch(url);
   const json = await raw.json();
-  const planetData = await json.results.map(async planet => {
-    const residents = await planet.residents.map(async resident => {
-      const raw = await fetch(resident);
-      const json = await raw.json();
-      return await json.name;
-    });
+  const planetData = json.results.map(async planet => {
+    const residents = await fetchResidents(planet);
     return ({name: planet.name, terrain: planet.terrain,
-      population:planet.population, climate:planet.climate,
-      residents:[...residents]});
+      population: planet.population, climate: planet.climate,
+      residents: [...residents]});
   });
   return Promise.all(planetData);
 };
 
-// const fetchResidents = async (planet) => {
-
-// }
+const fetchResidents = (planet) => {
+  const residents = planet.residents.map(async resident => {
+    const raw = await fetch(resident);
+    const json = await raw.json();
+    return json.name;
+  });
+  return Promise.all(residents);
+};
 
 export default fetchPlanets;
