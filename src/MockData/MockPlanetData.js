fdfdf0fa23b8131f0d03b9b81fbbaaa821d5921c
Charles Yach <CharlesY712@gmail.com>
2018-03-24 22:32:25 -0600

    Planet card tests

--- src/MockData/MockPlanetData.js
+++ src/MockData/MockPlanetData.js
@@ -1,5 +1,9 @@
 const MockPlanetData = {
-
+  climate: "temperate",
+  name: "Alderaan",
+  population: "2000000000",
+  residents:["Leia Organa", "Bail Prestor Organa", "Raymus Antilles"],
+  terrain: "grasslands, mountains"
 };
 
 export default MockPlanetData;
