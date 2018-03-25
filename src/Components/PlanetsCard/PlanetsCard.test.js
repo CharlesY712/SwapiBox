43269cf3973577449bc008d4f902747566a7c406
Charles Yach <CharlesY712@gmail.com>
2018-03-24 22:32:07 -0600

    Planet card tests

--- src/Components/PlanetsCard/PlanetsCard.test.js
+++ src/Components/PlanetsCard/PlanetsCard.test.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import PlanetsCard from './PlanetsCard';
+import MockPlanetData from '../../MockData/MockPlanetData';
 
 describe('PlanetsCard', () => {
-  const mockPlanetsData = 
+  const mockPlanetsData = MockPlanetData;
   let wrapper = shallow(<PlanetsCard 
-    name={planet.name}
-    terrain={planet.terrain}
-    population={planet.population}
-    climate={planet.climate}
-    residents={planet.residents}/>)
+    name={mockPlanetsData.name}
+    terrain={mockPlanetsData.terrain}
+    population={mockPlanetsData.population}
+    climate={mockPlanetsData.climate}
+    residents={mockPlanetsData.residents}/>);
+
+  it('should exist', () => {
+    expect(wrapper).toBeDefined();
+  });
+
+  it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
 });
